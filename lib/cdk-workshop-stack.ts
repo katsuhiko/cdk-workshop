a6ef220c0ccad0ac577472c2440ef92ddccd979a
katsuhiko <katsuhiko.nagashima@gmail.com>
2021-03-07 19:31:10 +0900

    余計な import が残っていたので削除しました。

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,3 @@
-import { LambdaSubscription } from '@aws-cdk/aws-sns-subscriptions';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
